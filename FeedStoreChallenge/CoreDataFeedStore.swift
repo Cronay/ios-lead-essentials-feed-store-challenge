22937e49fc0888eb63f18637936a8b0946cc37a6
Cronay <inf@yannic-borgfeld.de>
2020-09-28 19:10:26 +0200

    Deliver found values on retrieve command

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -26,11 +26,49 @@ public class CoreDataFeedStore: FeedStore {
     }
 
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        context.perform { [context] in
+            let cache = ManagedCache(context: context)
+            cache.timestamp = timestamp
+            let managedFeedArray = feed.map { (localImage) -> ManagedFeedImage in
+                let managedFeedImage = ManagedFeedImage(context: context)
+                managedFeedImage.id = localImage.id
+                managedFeedImage.imageDescription = localImage.description
+                managedFeedImage.location = localImage.location
+                managedFeedImage.url = localImage.url
+                return managedFeedImage
+            }
+            cache.images = NSOrderedSet(array: managedFeedArray)
 
+            do {
+                try context.save()
+                completion(nil)
+            } catch {
+                completion(error)
+            }
+        }
     }
 
     public func retrieve(completion: @escaping RetrievalCompletion) {
-        completion(.empty)
+        context.perform { [context] in
+            do {
+                let fetchedCaches = try context.fetch(ManagedCache.fetchRequest() as NSFetchRequest<ManagedCache>)
+
+                if let managedCache = fetchedCaches.first {
+                    let localFeed = managedCache.images!.compactMap { (managedImage) -> LocalFeedImage? in
+                        if let image = managedImage as? ManagedFeedImage {
+                            return LocalFeedImage(id: image.id!, description: image.imageDescription, location: image.location, url: image.url!)
+                        } else {
+                            return nil
+                        }
+                    }
+                    completion(.found(feed: localFeed, timestamp: managedCache.timestamp!))
+                } else {
+                    completion(.empty)
+                }
+            } catch {
+                completion(.failure(error))
+            }
+        }
     }
 }
 
